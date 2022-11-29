d8d02d23880c85947c609d9d13b516da4af2a423
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2022-11-29 13:59:59 +0700

    Setup environment

---
+++ src/modules.ts
@@ -0,0 +1,16 @@
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+export const Modules = [
+  ConfigModule.forRoot({}),
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    entities: [__dirname + '/model/entities/**/*{.ts,.js}'],
+    synchronize: true,
+  }),
+];
