ca2c0830cb3e2d74f59665908c2e06fb163284c5
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-01-26 21:10:07 +0700

    Feat: manufacturer

--- src/modules/manufacturer/manufacturer.controller.ts
+++ src/modules/manufacturer/manufacturer.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Post,
+  Put,
+  Body,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAdminAuthGuard } from '../auth/guards/jwt-admin-auth.guard';
 import { JwtAllAuthGuard } from '../auth/guards/jwt-all-auth.guard';
+import { ManufacturerDto } from './dtos/manufacturer.dto';
 import { ManufacturerService } from './manufacturer.service';
 
 @ApiTags('manufacturer')
@@ -12,5 +23,37 @@ export class ManufacturerController {
   @UseGuards(JwtAllAuthGuard)
   async getAllManufacturers() {
     return await this.manufacturerService.getAllManufacturers();
+  }
+
+  @Get('get-manufacturer-by-id')
+  @UseGuards(JwtAllAuthGuard)
+  async getManufacturerById(@Body('id', ParseIntPipe) id: number) {
+    return await this.manufacturerService.getManufacturerById(id);
+  }
+
+  @Post('create-manufacturer')
+  @UseGuards(JwtAdminAuthGuard)
+  async createManufacturer(@Body() manufacturerDto: ManufacturerDto) {
+    return await this.manufacturerService.createNewManufacturer(
+      manufacturerDto,
+    );
+  }
+
+  @Put('update-manufacturer')
+  @UseGuards(JwtAdminAuthGuard)
+  async updateManufacturer(
+    @Body() manufacturerDto: ManufacturerDto,
+    @Body('id', ParseIntPipe) id: number,
+  ) {
+    return await this.manufacturerService.updateManufacturer(
+      id,
+      manufacturerDto,
+    );
+  }
+
+  @Delete('delete-manufacturer')
+  @UseGuards(JwtAdminAuthGuard)
+  async deleteManufacturer(@Body('id', ParseIntPipe) id: number) {
+    return await this.manufacturerService.deleteManufacturer(id);
   }
 }
--- src/modules/manufacturer/manufacturer.service.ts
+++ src/modules/manufacturer/manufacturer.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Manufacturer } from 'src/models/entities/manufacturer.entity';
 import { ManufacturerRepository } from 'src/models/repositories/manufacturer.repository';
+import { ManufacturerDto } from './dtos/manufacturer.dto';
 
 @Injectable()
 export class ManufacturerService {
@@ -13,12 +14,39 @@ export class ManufacturerService {
   ) {}
 
   async getAllManufacturers() {
-    const categories = await this.manufacturerRepo.find();
-    return categories;
+    const manufacturers = await this.manufacturerRepo.find({
+      relations: { assetModels: true, licenses: true },
+    });
+    const res = manufacturers.map((manufacturer) => {
+      const { assetModels, licenses, ...rest } = manufacturer;
+      return {
+        ...rest,
+        numOfAssets: assetModels.length + licenses.length,
+      };
+    });
+    return res;
   }
 
   async getManufacturerById(id: number) {
     const manufacturer = await this.manufacturerRepo.findOneBy({ id });
     return manufacturer;
+  }
+
+  async createNewManufacturer(manufacturerDto: ManufacturerDto) {
+    const manufacturer = new Manufacturer();
+    manufacturer.name = manufacturerDto.name;
+    await this.manufacturerRepo.save(manufacturer);
+    return manufacturer;
+  }
+
+  async updateManufacturer(id: number, manufacturerDto: ManufacturerDto) {
+    let toUpdate = await this.manufacturerRepo.findOneBy({ id });
+
+    let updated = Object.assign(toUpdate, manufacturerDto);
+    return await this.manufacturerRepo.save(updated);
+  }
+
+  async deleteManufacturer(id: number) {
+    return await this.manufacturerRepo.delete({ id });
   }
 }
