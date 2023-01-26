bc15adc019c3d4d5d20fdcdb26e5d12d2a383843
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-01-26 21:33:42 +0700

    Feat: supplier

--- src/modules/supplier/supplier.controller.ts
+++ src/modules/supplier/supplier.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Post,
+  Put,
+  Body,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAdminAuthGuard } from '../auth/guards/jwt-admin-auth.guard';
 import { JwtAllAuthGuard } from '../auth/guards/jwt-all-auth.guard';
+import { SupplierDto } from './dtos/supplier.dto';
 import { SupplierService } from './supplier.service';
 
 @ApiTags('supplier')
@@ -12,5 +23,37 @@ export class SupplierController {
   @UseGuards(JwtAllAuthGuard)
   async getAllSuppliers() {
     return await this.supplierService.getAllSuppliers();
+  }
+
+  @Get('get-supplier-by-id')
+  @UseGuards(JwtAllAuthGuard)
+  async geSupplierById(@Body('id', ParseIntPipe) id: number) {
+    return await this.supplierService.getSupplierById(id);
+  }
+
+  @Post('create-supplier')
+  @UseGuards(JwtAdminAuthGuard)
+  async createSupplier(@Body() supplierDto: SupplierDto) {
+    return await this.supplierService.createNewSupplier(
+      supplierDto,
+    );
+  }
+
+  @Put('update-supplier')
+  @UseGuards(JwtAdminAuthGuard)
+  async updateSupplier(
+    @Body() supplierDto: SupplierDto,
+    @Body('id', ParseIntPipe) id: number,
+  ) {
+    return await this.supplierService.updateSupplier(
+      id,
+      supplierDto,
+    );
+  }
+
+  @Delete('delete-supplier')
+  @UseGuards(JwtAdminAuthGuard)
+  async deleteSupplier(@Body('id', ParseIntPipe) id: number) {
+    return await this.supplierService.deleteSupplier(id);
   }
 }
--- src/modules/supplier/supplier.service.ts
+++ src/modules/supplier/supplier.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Supplier } from 'src/models/entities/supplier.entity';
 import { SupplierRepository } from 'src/models/repositories/supplier.repository';
+import { SupplierDto } from './dtos/supplier.dto';
 
 @Injectable()
 export class SupplierService {
@@ -12,12 +13,39 @@ export class SupplierService {
   ) {}
 
   async getAllSuppliers() {
-    const suppliers = await this.supplierRepo.find();
-    return suppliers;
+    const suppliers = await this.supplierRepo.find({
+      relations: { assets: true, licenses: true },
+    });
+    const res = suppliers.map((supplier) => {
+      const { assets, licenses, ...rest } = supplier;
+      return {
+        ...rest,
+        numOfAssets: assets?.length??0 + licenses?.length??0,
+      };
+    });
+    return res;
   }
 
   async getSupplierById(id: number) {
     const supplier = await this.supplierRepo.findOneBy({ id });
     return supplier;
+  }
+
+  async createNewSupplier(supplierDto: SupplierDto) {
+    const supplier = new Supplier();
+    supplier.name = supplierDto.name;
+    await this.supplierRepo.save(supplier);
+    return supplier;
+  }
+
+  async updateSupplier(id: number, supplierDto: SupplierDto) {
+    let toUpdate = await this.supplierRepo.findOneBy({ id });
+
+    let updated = Object.assign(toUpdate, supplierDto);
+    return await this.supplierRepo.save(updated);
+  }
+
+  async deleteSupplier(id: number) {
+    return await this.supplierRepo.delete({ id });
   }
 }
