d2d17d00ebe2da2882c3965a2edb0cd163d0ca41
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-02-18 13:35:45 +0700

    Fix: add property

--- src/modules/asset/asset.service.ts
+++ src/modules/asset/asset.service.ts
@@ -479,6 +479,7 @@ export class AssetService {
           assetModel: asset?.assetModel?.name,
           department: asset?.department?.name,
           status: asset?.status?.name,
+          statusColor: asset?.status?.color,
           supplier: asset?.supplier?.name,
         };
       }),
