5d7212d7c26ae0b6fc0a330b694fca5380c92de7
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-03-09 00:19:58 +0700

    Fix: change password

--- src/modules/auth/dtos/change-passord.dto.ts
+++ src/modules/auth/dtos/change-passord.dto.ts
@@ -13,13 +13,6 @@ export class ChangePasswordDto {
   })
   @IsString()
   @IsNotEmpty()
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-    {
-      message:
-        'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character',
-    },
-  )
   newPassword: string;
 }
 
